81166c2899b7fd3d06dbdbf266a21ff1b1eb80d9
dev-timm <timm.kuemmeth@gmail.com>
2024-03-08 11:45:51 +0000

    add user profiles detail view

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,5 @@ from profiles import views
 
 urlpatterns = [
     path('profiles/', views.UserProfileList.as_view()),
+    path('profiles/<int:pk>/', views.UserProfileDetail.as_view()),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import UserProfile
@@ -9,3 +11,30 @@ class UserProfileList(APIView):
         serializer = UserProfileSerializer(profiles, many=True)
 
         return Response(serializer.data)
+
+
+class UserProfileDetail(APIView):
+    serializer_class = UserProfileSerializer
+
+    def get_object(self, pk):
+        try:
+            profile = UserProfile.objects.get(pk=pk)
+            return profile
+        except UserProfile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = UserProfileSerializer(profile)
+
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = UserProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
